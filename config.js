383ece444ee6a465d615b84247b2609f9b329aa6
Pim Chuaylua <pimchuaylua@Pims-MacBook-Air.local>
2026-01-06 09:36:29 +0700

    feat: can click and see event details, meeting number

---
+++ config.js
@@ -0,0 +1,4 @@
+window.CONFIG = {
+    BASE_URL: "https://api.lingpingclub.com",
+    API_KEY: "MZJ6NMliOBUhZcosXc6fgbeNV7H5r6rry6f8XhlW9/M9e54dDBNRNFojdZWcb7B8xf9htGeCkPwbIDtjM0HYp/aZcm29jm1RhFXq4mtjVlUAjgn1Ly7SdxEluXgK/Dof1wE39QIvLk6kal99V0XxVjNFrX+p37aUt0yQceOVbmg="  // your API key
+};
