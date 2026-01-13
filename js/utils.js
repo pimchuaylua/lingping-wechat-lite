cab296f214f9088cf72dcc14aaefa9606617ff80
Pim Chuaylua <pimchuaylua@Pims-MacBook-Air.local>
2026-01-13 11:18:40 +0700

    feat: add timezone

--- js/utils.js
+++ js/utils.js
@@ -2,11 +2,24 @@
 window.Utils = {
     formatPrettyDate(dateStr) {
         const [year, month, day] = dateStr.split("-").map(Number);
+
+        // Create date in local timezone
         const d = new Date(year, month - 1, day);
-        return d.toLocaleDateString("en-US", {
+
+        // Detect user's timezone
+        const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+
+        // Try to get a readable timezone name
+        const timeZoneName = new Intl.DateTimeFormat("en-US", {
+            timeZoneName: "long"
+        }).format(d).split(", ").pop();
+
+        const formattedDate = d.toLocaleDateString("en-US", {
             weekday: "short",
             day: "numeric",
             month: "short"
         });
+
+        return `${formattedDate} (${timeZoneName})`;
     }
 };
