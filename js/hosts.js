3ad696b2f405be330d23301c16b224149f42cced
Pim Chuaylua <pimchuaylua@Pims-MacBook-Air.local>
2026-01-19 23:57:16 +0700

    feat: meeting layout fixed

---
+++ js/hosts.js
@@ -0,0 +1,10 @@
+// js/hosts.js
+
+window.HOSTS = {
+    "pim001": {
+        userId: "pim001",
+        name: "Pim",
+        photo: "/assets/hosts/pim.jpg",
+        description: "Originally from Bangkok. Loves learning Chinese and reading."
+    }
+};
